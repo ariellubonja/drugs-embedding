eb529d478395a802b721ce7039f8fd1fac700f5f
Ariel Lubonja <ariel@cs.jhu.edu>
2023-07-20 10:25:05 +0300

    Do Embeddings for 6k molecules, needed to 7zip them

--- utils/drug_data_tools.py
+++ utils/drug_data_tools.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def get_selected_molecules():
+def get_selected_molecules(filter_molecules=True):
     """
     Returns list of molecules from CSV
     """
@@ -10,21 +10,24 @@ def get_selected_molecules():
     molecules_list = list(molecules['Gain'].str.lower())
     # selected_molecules = [m.split(" ")[-1] for m in molecules_list[0:7000] if (("+" not in m) and 
 
-    selected_molecules = [m.split(" ")[-1] for m in molecules_list[0:7000] if (("+" not in m) and 
-                        (m.endswith("olol") or 
-                        m.endswith('cillin') or 
-                        m.endswith('sartan') or 
-                        m.endswith('mycin') or 
-                        m.endswith('vir') or 
-                        m.endswith('parin') or
-                        m.endswith('mab') or 
-                        m.endswith('lamide') or
-                        m.endswith('caine') or
-                        m.endswith('bital') or
-                        m.endswith('afil') or
-                        m.endswith('asone') or
-                        m.endswith('profen') or 
-                        m.endswith('statin') or
-                        m.endswith('tinib')))]
+    if not filter_molecules:
+        selected_molecules = [m.split(" ")[-1] for m in molecules_list[0:7000] if ("+" not in m)]
+    else:
+        selected_molecules = [m.split(" ")[-1] for m in molecules_list[0:7000] if (("+" not in m) and 
+                (m.endswith("olol") or 
+                m.endswith('cillin') or 
+                m.endswith('sartan') or 
+                m.endswith('mycin') or 
+                m.endswith('vir') or 
+                m.endswith('parin') or
+                m.endswith('mab') or 
+                m.endswith('lamide') or
+                m.endswith('caine') or
+                m.endswith('bital') or
+                m.endswith('afil') or
+                m.endswith('asone') or
+                m.endswith('profen') or 
+                m.endswith('statin') or
+                m.endswith('tinib')))]
     
     return selected_molecules
